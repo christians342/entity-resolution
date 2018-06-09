9c1dc1b4d47c62fceb369af591a1368ab0b591f1
Christian Schmitz <ChristianSchmitz@uol.com.br>
2018-06-09 14:51:21 +0200

    Trying a naive approach where the similarity measure is made only by the publications titles. Also using naive approach comparing all publications to all publications when searching for matches.

--- EntityResolution.py
+++ EntityResolution.py
@@ -1,8 +1,35 @@
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('cora-all-id.xml')
-root = tree.getroot()
+def match(publication1, publication2):
+    publication1TitlesList = list(map(lambda x: x.text, publication1.findall("title")))
+    publication1TitlesList = list(map(lambda x: x.lower(), publication1TitlesList))
+    publication1FullTitleString = " ".join(publication1TitlesList)
+    publication1FullTitleString.lower()
 
-for child in root:
-    print(child)
-    
+    publication2TitlesList = list(map(lambda x: x.text, publication2.findall("title")))
+    publication2TitlesList = list(map(lambda x: x.lower(), publication2TitlesList))
+    publication2FullTitleString = " ".join(publication2TitlesList)
+    publication2FullTitleString.lower()
+
+    if publication1FullTitleString == publication2FullTitleString:
+        return True
+    return False
+
+def entityResolution(publications):
+    matches = 0
+    for i in range(0, len(publications)):
+        for j in range(i + 1, len(publications)):
+            if match(publications[i], publications[j]):
+                #print("Deu match entre entities " + str(publications[i]) + str(publications[j]))
+                matches+=1
+    print(matches)
+    print(len(publications))
+
+
+
+if __name__ == '__main__':
+
+    tree = ET.parse('cora-all-id.xml')
+    root = tree.getroot()
+
+    entityResolution(root)
